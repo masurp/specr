5c11967e65ce11cc79c1e95eb0c7f544af1940e9
Philipp Masur <philippmasur@eduroam-86-78.zdv.Uni-Mainz.DE>
2019-11-20 16:29:49 +0100

    Update to documentation.

--- R/run_specs.r
+++ R/run_specs.r
@@ -1,18 +1,20 @@
 #' Run specification curve analysis
 #'
-#' @param df
-#' @param y
-#' @param x
-#' @param model
-#' @param controls
-#' @param subsets
+#' @param df a data frame that includes all relevant variables
+#' @param y a vector of the dependent variables
+#' @param x a vevtor of the dependent variables
+#' @param model a vector of the type of models that should be estimated
+#' @param controls a vector of the control variables that should be included
+#' @param subsets a list that includes named vectors
 #'
 #' @return
 #' @export
 
 run_specs <- function(df, y, x, model, controls, subsets) {
+
   specs <- setup_specs(y = y, x = x, model = model, controls = controls)
   df_list <- create_subsets(df, subsets)
   map_df(df_list, ~ run_spec(specs, .x) %>%
     mutate(subset = unique(.x$filter)))
+
 }
