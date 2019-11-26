aa4a9a17f8a967672a30077a6082f66877c3bbef
Philipp Masur <philippmasur@eduroam-219-135.zdv.Uni-Mainz.DE>
2019-11-26 13:47:36 +0100

    Create a simulated data set for illustration purposes.

---
+++ R/example_data.4.R
@@ -0,0 +1,17 @@
+#' Simulated data set
+#'
+#' This data set can be used to illustrate the major function of "specr".
+#'
+#' @docType data
+#'
+#' @usage data(example_data)
+#'
+#' @format A tibble
+#'
+#' @keywords datasets
+#
+#'
+#' @examples
+#' data(example_data)
+#' head(example_data)
+"example_data"
