a81b390e6cddb5dd68408cb165006ac4951c6265
Philipp Masur <philippmasur@eduroam-219-135.zdv.Uni-Mainz.DE>
2019-11-26 14:15:38 +0100

    Updated description and included example.

--- R/plot_specs.r
+++ R/plot_specs.r
@@ -1,5 +1,10 @@
 #' Plot specification curve and analytical choices
 #'
+#' This function plots an entire visualization of the specification curve analysis. It can be used in two ways:
+#' 1. The results frame returned by \code{run_specs()} is passed directly. In this case, the function plots the entire visualization automatically.
+#' 2. The ggplot objects returned from \code{plot_curve()} and \code{plot_choices()} are passed to the function. In this case, the function simply arranges them above each other.
+#'
+#'
 #' @param df data frame resulting from \code{run_specs()}.
 #' @param plot_a a ggplot object resulting from \code{plot_curve()}.
 #' @param plot_b a ggplot object resulting from \code{plot_choices()}.
@@ -9,10 +14,37 @@
 #' @param ci logical value indicating whether confidence intervals should be plotted.
 #' @param prob numeric value indicating what type of confidence intervals should be plotted. Defaults to .975 (= 95% confidence intervalls.)
 #'
-#' @return
+#' @return a gg/ggplot object.
 #' @export
 #'
 #' @examples
+#' # load data
+#' d <- example_data
+#'
+#' # run spec analysis
+#' results <- run_specs(d,
+#'                      y = c("y1", "y2"),
+#'                      x = c("x1", "x2"),
+#'                      model = "lm",
+#'                      controls = c("c1", "c2"),
+#'                      subset = list(gender = unique(d$gender)))
+#'
+#' # plot results directly
+#' plot_specs(results)
+#'
+#' # customize each part and then combine
+#' p1 <- plot_curve(results) +
+#'   geom_hline(yintercept = 0, linetype = "dashed", color = "grey") +
+#'   ylim(-3, 12) +
+#'   labs(x = "", y = "unstandarized regression coefficient")
+#'
+#' p2 <- plot_choices(results) +
+#'   labs(y = "analytical choices")
+#'
+#' plot_specs(plot_a = p1,
+#'            plot_b = p2,
+#'            rel_height = c(2, 2))
+#'
 plot_specs <- function(df = NULL,
                        plot_a = NULL,
                        plot_b = NULL,
