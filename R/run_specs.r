5e8ddf6f313f0e2fb03d150ceac97d4639c1e7d5
Philipp Masur <philippmasur@eduroam-219-135.zdv.Uni-Mainz.DE>
2019-11-26 14:15:19 +0100

    Updated description and included examples.

--- R/run_specs.r
+++ R/run_specs.r
@@ -1,13 +1,15 @@
 #' Run specification curve analysis
 #'
+#' This is the central function of the package. It can be used to run the specification curve analyses. It takes the data frame and vectors for analytical choices related to the dependent variable, the independent variable, the type of models that should be estimated, the set of covariates that should be included (none, each individually, and all together), as well as a named list of potential subsets. It returns a "result frame" which includes relevant statistics for each model as well as the analytical choices as factorial variables.
+#'
 #' @param df a data frame that includes all relevant variables
 #' @param y a vector of the dependent variables
 #' @param x a vevtor of the dependent variables
-#' @param model a vector of the type of models that should be estimated
+#' @param model a vector of the type of models that should be estimated.
 #' @param controls a vector of the control variables that should be included
 #' @param subsets a list that includes named vectors
 #'
-#' @return
+#' @return a tibble including all choices and results
 #' @export
 
 run_specs <- function(df, y, x, model, controls, subsets) {
