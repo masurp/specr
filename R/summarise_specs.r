e0eb0c446e346f5d135e75c064281226058e74a0
Philipp K. Masur <philipp.masur@uni-mainz.de>
2019-12-10 14:02:10 +0100

    Different vars can be evaluated as well as several grouping variables be passed.

--- R/summarise_specs.r
+++ R/summarise_specs.r
@@ -3,42 +3,49 @@
 #' This function allows to inspect results of the specification curves by returning a comparatively simple summary of the results. These results can be returned for specific analytical choices.
 #'
 #' @param df a data frame containing the choices and results of each specification (resulting from \code{run_specs}).
-#' @param group a grouping factor (e.g., "subsets"). Defaults to NULL.
+#' @param var which variable should be evaluated? Defaults to estimate (the effect sizes computed by \code{run_specs}). No need to use closures.
+#' @param group a grouping factor (e.g., "subsets"). Several grouping variables can be passed. Defaults to NULL.
+
 #'
 #' @return
 #' @export
 #'
 #' @examples
 summarise_specs <- function(df,
+                            var = estimate,
                             group = NULL) {
 
   require(dplyr)
 
+
   # Internal function
   summary_specs <- function(df) {
 
+    var <- enquo(var)
+
     df %>%
-    summarize(mean = mean(estimate),
-              sd = sd(estimate),
-              median = median(estimate),
-              mad = mad(estimate),
-              min = min(estimate),
-              max = max(estimate),
-              obs_med = median(obs))
+      summarize(mean = mean(!! var),
+                sd = sd(!! var),
+                median = median(!! var),
+                mad = mad(!! var),
+                min = min(!! var),
+                max = max(!! var),
+                obs_med = median(obs))
   }
 
 
   if (is_null(group)) {
 
-  df %>%
+    df %>%
       summary_specs
 
 
   } else {
-    group <- as.name(group)
+
+    group <- lapply(group, as.symbol)
 
     df %>%
-      group_by(!!group) %>%
+      group_by_(.dots = group) %>%
       summary_specs
   }
 
