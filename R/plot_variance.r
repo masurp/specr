0b9b17af1aec7d6d9d23944d4d9c53cc26e7399d
Philipp K. Masur <philipp.masur@uni-mainz.de>
2019-12-10 18:14:12 +0100

    New function icc_specs() instead of variance_specs() as there are too many ways to specify the multilevel model.

--- R/plot_variance.r
+++ R/plot_variance.r
@@ -1,17 +1,29 @@
 #' Plot variance components of the specification curve
 #'
-#' @param df a data frame containing the choices and results of each specification (resulting from \code{run_specs}).
+#' @param model a multilevel model that captures the variances of the specification curve (resulting from \code{run_specs}).
 #'
 #' @return
 #' @export
 #'
 #' @examples
-plot_variance <- function(df) {
+#' # Step 1: Run spec curve analysis
+#' results <- run_specs(df = example_data,
+#'                      y = c("y1", "y2"),
+#'                      x = c("x1", "x2"),
+#'                      model = c("lm"))
+#'
+#' # Step 2: Estimate multilevel model
+#' library(lme4)
+#' model <- lmer(estimate ~ 1 + (1|x)  + (1|y), data = results)
+#'
+#' # Step 3: Plot model
+#' plot_variance(model)
+plot_variance <- function(model) {
 
   # dependencies
   require(ggplot2)
 
-  variance_specs(df) %>%
+  icc_specs(model) %>%
     ggplot(aes(x = grp,
                y = percent)) +
     geom_bar(stat = "identity", fill = "#377eb8") +
