0a49c56e5f8cb802978f4fd882ce01f31b77fb61
Philipp K. Masur <philipp.masur@uni-mainz.de>
2019-12-10 18:15:49 +0100

    Update to function to better fit varying purposes.

--- R/summarise_specs.r
+++ R/summarise_specs.r
@@ -4,6 +4,7 @@
 #'
 #' @param df a data frame containing the choices and results of each specification (resulting from \code{run_specs}).
 #' @param var which variable should be evaluated? Defaults to estimate (the effect sizes computed by \code{run_specs}). No need to use closures.
+#' @param stats list object (\code{lst()}) of summary functions.
 #' @param group a grouping factor (e.g., "subsets"). Several grouping variables can be passed. Defaults to NULL.
 
 #'
@@ -13,40 +14,35 @@
 #' @examples
 summarise_specs <- function(df,
                             var = estimate,
+                            stats = lst(median, mad, min, max,
+                                        q25 = function(x) quantile(x, prob = .25),
+                                        q75 = function(x) quantile(x, prob = .75)),
                             group = NULL) {
 
   require(dplyr)
 
-
-  # Internal function
   summary_specs <- function(df) {
-
     var <- enquo(var)
-
     df %>%
-      summarize(mean = mean(!! var),
-                sd = sd(!! var),
-                median = median(!! var),
-                mad = mad(!! var),
-                min = min(!! var),
-                max = max(!! var),
-                obs_med = median(obs))
+      summarize_at(vars(!!var), stats)
   }
-
 
   if (is_null(group)) {
-
-    df %>%
-      summary_specs
-
-
+     bind_cols(
+       df %>%
+         summary_specs,
+       df %>%
+         summarize(obs = median(obs))
+     )
   } else {
-
     group <- lapply(group, as.symbol)
-
-    df %>%
-      group_by_(.dots = group) %>%
-      summary_specs
+    left_join(
+      df %>%
+        group_by_(.dots = group) %>%
+        summary_specs,
+      df %>%
+        group_by_(.dots = group) %>%
+        summarize(obs = median(obs))
+    )
   }
-
 }
