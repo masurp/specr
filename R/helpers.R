b1add0e42749b033162e92c1a2f982863d329c06
Philipp K. Masur <philipp.masur@uni-mainz.de>
2020-03-22 19:19:54 +0100

    Update code and cosmetics.

--- R/helpers.R
+++ R/helpers.R
@@ -10,60 +10,55 @@ create_formula <- function(x, y, controls, ...) {
 # run individual specification
 run_spec <- function(specs, df, conf.level, keep.results = FALSE) {
 
-  # dependencies
-  require(dplyr)
   require(purrr)
 
   results <- specs %>%
-    mutate(formula = pmap(., create_formula)) %>%
+    dplyr::mutate(formula = pmap(., create_formula)) %>%
     tidyr::unnest(formula) %>%
-    mutate(res = map2(model, formula, ~ do.call(.x, list(data = df, formula = .y)))) %>%
-    mutate(coefs = map(res, broom::tidy, conf.int = TRUE, conf.level = conf.level),
-           obs = map(res, nobs)) %>%
+    dplyr::mutate(res = map2(model, formula, ~ do.call(.x, list(data = df, formula = .y)))) %>%
+    dplyr::mutate(coefs = map(res, broom::tidy, conf.int = TRUE, conf.level = conf.level),
+                  obs = map(res, nobs)) %>%
     tidyr::unnest(coefs) %>%
     tidyr::unnest(obs) %>%
-    filter(term == x) %>%
-    select(-formula, -term)
+    dplyr::filter(term == x) %>%
+    dplyr::select(-formula, -term)
 
   if (isFALSE(keep.results)) {
     results <- results %>%
-      select(-res)
+      dplyr::select(-res)
   }
 
   return(results)
 }
 
-# create subsets
+# creates subsets
 create_subsets <- function(df, subsets) {
-
-  # dependencies
-  require(dplyr)
 
   subsets %>%
     stack %>%
-    purrr::pmap(~ filter(df, get(as.character(..2)) == ..1) %>%
-                  mutate(filter = paste(..2, "=", ..1)))
+    purrr::pmap(~ dplyr::filter(df, get(as.character(..2)) == ..1) %>%
+                  dplyr::mutate(filter = paste(..2, "=", ..1)))
 }
 
 
+# formats results
 format_results <- function(df, null = 0, desc = FALSE) {
-
-  # dependencies
-  require(dplyr)
 
   # rank specs
   if (isFALSE(desc)) {
     df <- df %>%
-      arrange(estimate)
+      dplyr::arrange(estimate)
   } else {
     df <- df %>%
-      arrange(desc(estimate))
+      dplyr::arrange(desc(estimate))
   }
+
+  # create rank variable and color significance
   df <- df %>%
-    mutate(specifications = 1:n(),
-           color = case_when(conf.low > null ~ "#377eb8",
-                             conf.high < null ~ "#e41a1c",
-                             TRUE ~ "darkgrey"))
+    dplyr::mutate(specifications = 1:n(),
+                  color = case_when(conf.low > null ~ "#377eb8",
+                                    conf.high < null ~ "#e41a1c",
+                                    TRUE ~ "darkgrey"))
   return(df)
 }
 
