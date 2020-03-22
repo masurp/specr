8c9bdd7e9c5189ee933fc69caf602c26e973b0fa
Philipp K. Masur <philipp.masur@uni-mainz.de>
2020-03-22 22:40:45 +0100

    Fix dependencies.

--- R/specr-package.r
+++ R/specr-package.r
@@ -1,8 +1,8 @@
 ## usethis namespace: start
-#' @import ggplot2
+#' @import ggplot2 dplyr
 #' @importFrom lme4 VarCorr
-#' @importFrom tibble tibble as_tibble lst
-#' @importFrom dplyr n
+#' @importFrom tibble tibble as_tibble
+#' @importFrom dplyr n lst enquo case_when
 #' @importFrom igraph graph_from_data_frame
 #' @importFrom purrr map map2 map_df reduce cross
 ## usethis namespace: end
