f35bf412feb9e9ae9888c4fba4bc7415da72c14c
Philipp K. Masur <philipp.masur@uni-mainz.de>
2020-03-22 20:39:42 +0100

    Import n() from dplyr.

--- R/specr-package.r
+++ R/specr-package.r
@@ -2,6 +2,7 @@
 #' @import ggplot2
 #' @importFrom lme4 VarCorr
 #' @importFrom tibble tibble as_tibble lst
+#' @importFrom dplyr n
 #' @importFrom igraph graph_from_data_frame
 #' @importFrom purrr map map2 map_df reduce cross
 ## usethis namespace: end
