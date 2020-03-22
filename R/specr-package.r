c201a89c80d8b907d775ab5385e25db632b6c30d
Philipp K. Masur <philipp.masur@uni-mainz.de>
2020-03-22 20:14:09 +0100

    Changed imports.

---
+++ R/specr-package.r
@@ -0,0 +1,8 @@
+## usethis namespace: start
+#' @import ggplot2
+#' @importFrom lme4 VarCorr
+#' @importFrom tibble tibble as_tibble lst
+#' @importFrom igraph graph_from_data_frame
+#' @importFrom purrr map map2 map_df reduce cross
+## usethis namespace: end
+NULL
