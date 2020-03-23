3718cfc8e0bf28657ba80996e0c2c24a5771a6e4
Philipp K. Masur <philipp.masur@uni-mainz.de>
2020-03-23 21:48:32 +0100

    Update imports and suggests.

--- R/specr-package.r
+++ R/specr-package.r
@@ -1,9 +1,12 @@
 ## usethis namespace: start
-#' @import ggplot2 dplyr
+#' @import ggplot2
 #' @importFrom lme4 VarCorr
-#' @importFrom tibble tibble as_tibble
-#' @importFrom dplyr n lst enquo case_when
+#' @importFrom dplyr case_when n
+#' @importFrom rlang enquo enquos .data
+#' @importFrom tibble tibble as_tibble lst
 #' @importFrom igraph graph_from_data_frame
-#' @importFrom purrr map map2 map_df reduce cross
+#' @importFrom purrr map map2 map_df pmap reduce cross
+#' @importFrom stats formula mad median nobs quantile start vcov
+#' @importFrom utils stack
 ## usethis namespace: end
 NULL
