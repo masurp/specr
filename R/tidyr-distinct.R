cca6873cd24fb3dd14f83bdc12ef0e41951b0e21
masurp <p.k.masur@vu.nl>
2022-12-20 20:45:29 +0100

    Import distinct to make subset creation possible.

---
+++ R/tidyr-distinct.R
@@ -0,0 +1,10 @@
+#' Subset distinct/unique rows
+#'
+#' See \code{dplyr::\link[dplyr:distinct]{distinct}} for details.
+#'
+#' @name distinct
+#' @rdname distinct
+#' @keywords internal
+#' @export
+#' @importFrom dplyr distinct
+NULL
