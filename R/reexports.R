c240356fb58a67c87c9513c9e39511f6aeaf4523
masurp <p.k.masur@vu.nl>
2023-01-11 19:15:10 +0100

    Delete single pages of reexports, now all in one R file.

---
+++ R/reexports.R
@@ -0,0 +1,20 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom furrr furrr_options
+#' @export
+furrr::furrr_options
+
+#' @importFrom tibble tibble
+#' @export
+tibble::as_tibble
+
+#' @importFrom dplyr distinct
+#' @export
+dplyr::distinct
+
+#' @importFrom cowplot plot_grid
+#' @export
+cowplot::plot_grid
+
